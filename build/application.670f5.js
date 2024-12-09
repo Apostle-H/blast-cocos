fcd1ebdd1e2c120ed57cd3d1316f775a092b65fa
Nikita Malinovskiy <65463842+Apostle-H@users.noreply.github.com>
2024-12-09 19:39:22 +0200

    tweak: build resolution

--- build/application.670f5.js
+++ build/application.670f5.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.378fb.json';
+          this.settingsPath = 'src/settings.0205c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
